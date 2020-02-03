76aeaace79ac4f8c5ccaf096c28247b675da503f
lbd1607 <35612977+lbd1607@users.noreply.github.com>
2020-02-03 17:57:18 -0600

    React setup

---
+++ releasenotes/frontend/src/components/notes/Notes.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Notes extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Release Notes List</h1>
+            </div>
+        )
+    }
+}
+
+export default Notes
